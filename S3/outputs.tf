80d6968267a03a49a2a75de9b9b3bbf4b9d0cd25
Firas Bouchedakh <firas.bouchedakh@esprit.tn>
2024-06-26 10:42:57 +0200

    Update outputs.tf

--- S3/outputs.tf
+++ S3/outputs.tf
@@ -3,7 +3,7 @@ output "s3_bucket_id" {
   value       = aws_s3_bucket.data_bucket.id
 }
 
-/* output "kms_key_id" {
+ output "kms_key_id" {
   description = "The ID of the KMS key"
   value       = aws_kms_key.S3_data_bucket_key.id
 }
@@ -12,4 +12,4 @@ output "kms_role_arn" {
   description = "The ARN of the KMS role"
   value       = aws_iam_role.kms_role.arn
 }
-  */
+  
